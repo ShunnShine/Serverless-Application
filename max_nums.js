bbd7ca1d3e97ef99dbcd77aec79245ea5be7b506
Shunn Shine <jack.shunn@gmail.com>
2022-04-26 05:02:27 -0600

    max function

--- max_nums.js
+++ max_nums.js
@@ -1,1 +1,11 @@
-// start coding your function here!
+function find_max(arr){
+    let max = arr[0];
+    arr.forEach(element => {
+        if (element > max){
+            max = element
+        }
+    });
+    return max;
+}
+
+exports.find_max = find_max;
